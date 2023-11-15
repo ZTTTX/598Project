21fa2a0fd4e1981105579a8fca5ad570dde16cbf
kh-ryu <kr37@illinois.edu>
2023-11-15 11:55:28 -0600

    add prompt

--- gpt_api/gpt_api.py
+++ gpt_api/gpt_api.py
@@ -2,6 +2,11 @@ from openai import OpenAI
 import yaml
 from pathlib import Path
 from playsound import playsound
+from typing import Optional
+
+def file_to_string(filename):
+    with open(filename, 'r') as file:
+        return file.read()
 
 def speech2text(client: OpenAI, data_folder, file_name = "test.mp3"):
     audio_file= open(data_folder / file_name, "rb")
@@ -23,38 +28,44 @@ def text2speech(client: OpenAI, data_folder, text: str = "Hello, World!"):
     response.stream_to_file(speech_file_path)
     return response, speech_file_path
 
-if __name__ == "__main__":
-    system_prompt = "You are a helpful assistant"
-
-    # Get the full filepath of the current script
+def one_step_conversation(message_history: list, emotion: str = "neutral"):
+    # Set up directories
     current_file = Path(__file__).resolve()
     current_directory = current_file.parent
     parant_directory = current_directory.parent
     data_folder = parant_directory / "data"
-    print("Data folder: ", data_folder)
 
-    # Open the YAML file
+    # Create the client
     with open(current_directory/'key.yaml', 'r') as file:
         config = yaml.safe_load(file)
-
-    # Create the OpenAI client
     client = OpenAI(api_key=config['api_key'])
 
+    if message_history == []:
+        # Initialize the message with system prompt
+        system_prompt = file_to_string(current_directory / "system_prompt.txt")
+        message = {"role": "system", "content": system_prompt}
+        message_history.append(message)
+
+    # Add user message to the message history
+    user_prompt = file_to_string(current_directory / "user_prompt.txt")
     # Get the transcript
     transcript = speech2text(client, data_folder)
-    user_message = transcript
-    print("User input: ",transcript)
+    user_message = {"role": "user", "content": user_prompt.format(EMOTION=emotion, DIALOGUE=transcript)}
+    message_history.append(user_message)
+    print("User emotion: ",emotion)
+    print("User message: ",transcript)
 
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_message},
-        ]
+        messages=message_history,
     )
 
     print("GPT output: ", response.choices[0].message.content)
+    assistant_message = {"role": "assistant", "content": response.choices[0].message.content}
+    message_history.append(assistant_message)
 
     # Get the speech
     response, mp3_file_path = text2speech(client, data_folder, response.choices[0].message.content)
     playsound(mp3_file_path)
+
+    return message_history
